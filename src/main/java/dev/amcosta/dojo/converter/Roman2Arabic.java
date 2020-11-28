fb7c6404064dca79db9ddcc171c20f972f792408
Alex Moreno <alex.moreno.costa@gmail.com>
2020-11-28 12:18:57 -0300

    conversão funcionando

--- src/main/java/dev/amcosta/dojo/converter/Roman2Arabic.java
+++ src/main/java/dev/amcosta/dojo/converter/Roman2Arabic.java
@@ -1,16 +1,21 @@
 package dev.amcosta.dojo.converter;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Roman2Arabic {
 
     String symbols;
+    char[] characters;
+    Integer pointer = 0;
 
     Map<String, Integer> dictionary = new HashMap<>();
 
     public Roman2Arabic(String symbols) {
         this.symbols = symbols;
+        this.characters = symbols.toCharArray();
 
         this.dictionary.put("I", 1);
         this.dictionary.put("V", 5);
@@ -19,9 +24,24 @@ public class Roman2Arabic {
         this.dictionary.put("C", 100);
         this.dictionary.put("D", 500);
         this.dictionary.put("M", 1000);
+
+
     }
 
     public int convert() {
-        return this.dictionary.get(this.symbols);
+        Integer total = 0;
+
+        for (int i = 0; i < this.characters.length; i++) {
+            Integer currentValue = this.dictionary.getOrDefault(String.valueOf(this.characters[i]), 0);
+            Integer previousValue = i == 0 ? 0 : this.dictionary.getOrDefault(String.valueOf(this.characters[i - 1]), 0);
+
+            if (currentValue > previousValue) {
+                total = currentValue - previousValue;
+            } else {
+                total += currentValue;
+            }
+        }
+
+        return total;
     }
 }
--- src/test/java/dev/amcosta/dojo/converter/Roman2ArabicTest.java
+++ src/test/java/dev/amcosta/dojo/converter/Roman2ArabicTest.java
@@ -60,4 +60,109 @@ public class Roman2ArabicTest {
         Roman2Arabic converter = new Roman2Arabic(numbers);
         Assert.assertEquals(2, converter.convert());
     }
+
+    @Test
+    public void testIIIEquals3() {
+        String numbers = "III";
+        Roman2Arabic converter = new Roman2Arabic(numbers);
+        Assert.assertEquals(3, converter.convert());
+    }
+
+    @Test
+    public void testXXEquals20() {
+        String numbers = "XX";
+        Roman2Arabic converter = new Roman2Arabic(numbers);
+        Assert.assertEquals(20, converter.convert());
+    }
+
+    @Test
+    public void testXXXEquals30() {
+        String numbers = "XXX";
+        Roman2Arabic converter = new Roman2Arabic(numbers);
+        Assert.assertEquals(30, converter.convert());
+    }
+
+    @Test
+    public void testCCEquals200() {
+        String numbers = "CC";
+        Roman2Arabic converter = new Roman2Arabic(numbers);
+        Assert.assertEquals(200, converter.convert());
+    }
+
+    @Test
+    public void testCCCEquals300() {
+        String numbers = "CCC";
+        Roman2Arabic converter = new Roman2Arabic(numbers);
+        Assert.assertEquals(300, converter.convert());
+    }
+
+    @Test
+    public void testMMEquals2000() {
+        String numbers = "MM";
+        Roman2Arabic converter = new Roman2Arabic(numbers);
+        Assert.assertEquals(2000, converter.convert());
+    }
+
+    @Test
+    public void testMMMEquals3000() {
+        String numbers = "MMM";
+        Roman2Arabic converter = new Roman2Arabic(numbers);
+        Assert.assertEquals(3000, converter.convert());
+    }
+
+    @Test
+    public void testIVEquals4() {
+        String numbers = "IV";
+        Roman2Arabic converter = new Roman2Arabic(numbers);
+        Assert.assertEquals(4, converter.convert());
+    }
+
+    @Test
+    public void testIXEquals10() {
+        String numbers = "IX";
+        Roman2Arabic converter = new Roman2Arabic(numbers);
+        Assert.assertEquals(9, converter.convert());
+    }
+
+    @Test
+    public void testXLEquals40() {
+        String numbers = "XL";
+        Roman2Arabic converter = new Roman2Arabic(numbers);
+        Assert.assertEquals(40, converter.convert());
+    }
+
+    @Test
+    public void testXCEquals90() {
+        String numbers = "XC";
+        Roman2Arabic converter = new Roman2Arabic(numbers);
+        Assert.assertEquals(90, converter.convert());
+    }
+
+    @Test
+    public void testVIIIEquals8() {
+        String numbers = "VIII";
+        Roman2Arabic converter = new Roman2Arabic(numbers);
+        Assert.assertEquals(8, converter.convert());
+    }
+
+    @Test
+    public void testLXIIEquals62() {
+        String numbers = "LXII";
+        Roman2Arabic converter = new Roman2Arabic(numbers);
+        Assert.assertEquals(62, converter.convert());
+    }
+
+    @Test
+    public void testCLVIIIEquals158() {
+        String numbers = "CLVIII";
+        Roman2Arabic converter = new Roman2Arabic(numbers);
+        Assert.assertEquals(158, converter.convert());
+    }
+
+    @Test
+    public void testMCXXEquals1120() {
+        String numbers = "MCXX";
+        Roman2Arabic converter = new Roman2Arabic(numbers);
+        Assert.assertEquals(1120, converter.convert());
+    }
 }
